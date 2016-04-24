3fcc3683d904607cfacbb69127495209ca21ab92
Eman <emmanuoel.h@gmail.com>
2016-04-24 00:12:19 -0700

    Add TabBar, NavigationController, and SentMemeViewcontroller

---
+++ Meme Me/SentMemesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SentMemesViewController.swift
+//  Meme Me
+//
+//  Created by Emmanuoel Haroutunian on 4/23/16.
+//  Copyright © 2016 Emmanuoel Haroutunian. All rights reserved.
+//
+
+import UIKit
+
+class SentMemesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
